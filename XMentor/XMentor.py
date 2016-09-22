754559c29da74a8c63bd357d19bc786e77bcad60
NoahChanInvictus <noah_chan@outlook.com>
2016-09-22 21:52:50 +0800

    first commit

--- XMentor/XMentor.py
+++ XMentor/XMentor.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
-import sys
 import urllib2
-#import ngender
-
-sys.path.append('../ngender/ngender/')
 import ngender
 
 url_base = 'http://sem.buaa.edu.cn/szdw/'
@@ -15,28 +11,52 @@ def get_mainpage_info():
     url = url_base + 'jsbd.htm'
     response = urllib2.urlopen(url)
     html = response.read()
-    print html
+    #print html
     url_pool = []
     comp = re.compile(r'<div class="subNav">  <a href="(.*?)"')
     url_pool = comp.findall(html)
-    print url_pool
+    #print url_pool
     return url_pool
 
 def get_subpage_info(url_pool):
+    mentor_list = []
     for sub_url in url_pool:
         url = url_base + sub_url
         response = urllib2.urlopen(url)
-        html = response.read().decode('utf-8')
+        html = response.read()
         #print html
+        #print type(html)
         comp = re.compile(r'<span style="font-family: 宋体">(.*?)</span></span></strong>')
         names = comp.findall(html)
-        #for name in names:
-            #print name.split('(')
-
-
-        print len(names)
-        return 0
+        for name in names:
+            mentor = {}
+            name,title = name.split(r'（') 
+            title = title.rstrip('）')
+            gender = ngender.guess(name)
+            mentor['name'] = name
+            mentor['gender'] = gender
+            mentor['title'] = title
+            mentor['department'] = sub_url
+            mentor_list.append(mentor)
+        print url
+        if len(names) != 0:
+            continue
+        else:
+            comp = re.compile(r'<div align="center" style="height: 30px">(.*?)</div>')
+            names = comp.findall(html)
+            for name in names:
+                mentor = {}
+                mentor['name'] = name
+                gender = ngender.guess(name)
+                mentor['gender'] = gender
+                mentor['title'] = 'unknown'
+                mentor['department'] = sub_url
+                mentor_list.append(mentor)
+    return mentor_list
+        
 
 if __name__ == '__main__':
     url_pool = get_mainpage_info()
-    get_subpage_info(url_pool)
+    mentors = get_subpage_info(url_pool)
+    print mentors
+    print len(mentors)
